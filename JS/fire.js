edc85c143e68ffe532de3ead5d36949786cc6d82
Carson Riker <c@rsonriker.com>
2019-04-17 13:35:54 -0400

    Add files via upload

--- JS/fire.js
+++ JS/fire.js
@@ -46,24 +46,15 @@ var titleFires = [
      String.raw`((/ __|((_)_  ((_)((_) ((_) _(_/(  | _ \ (_)| |(_)(_))   ((_) `,
      String.raw` | (__ / _' || '_|(_-</ _ \| '  \))|   / | || / / / -_) | '_| `,
      String.raw`  \___|\__,_||_|  /__/\___/|_||_|  |_|_\ |_||_\_\ \___| |_|   `,
-     String.raw`                                                              `],
-    [String.raw`                                    (                         `,
-     String.raw`   (                                )\ )        )             `,
-     String.raw`   )\      )  (                    (()/( (   ( /(    (   (    `,
-     String.raw` (((_)  ( /(  )(   (    (    (      /(_)))\  )\())  ))\  )(   `,
-     String.raw` )\___  )(_))(()\  )\   )\   )\ )  (_)) ((_)((_)\  /((_)(()\  `,
-     String.raw`((/ __|((_)_  ((_)((_) ((_) _(_/(  | _ \ (_)| |(_)(_))   ((_) `,
-     String.raw` | (__ / _' || '_|(_-</ _ \| '  \))|   / | || / / / -_) | '_| `,
-     String.raw`  \___|\__,_||_|  /__/\___/|_||_|  |_|_\ |_||_\_\ \___| |_|   `,
      String.raw`                                                              `]
 ];
-
-var bottom = document.getElementById("bottomFire");
 
 function initFire(){
     changeFire();
     setBottom();
-    setInterval(function(){ changeFire(); }, 150);
+    setInterval(function(){ changeFire(); }, 200);
+    setInterval(function(){ setBottom(); }, 150);
+    window.addEventListener('resize', function(){setBottom()});
 }
 
 function changeFire(){
@@ -80,22 +71,39 @@ function fireString(array){
     return str;
 }
 
-window.addEventListener('resize', function(){setBottom()});
+
+
+var bottom = document.getElementById("bottomFire");
+var bottomState = 0;
+var bottomFires = [
+    ["   ) ",
+     " ( /(",
+     ")(/))",
+     "((_)\\"],
+    ["   ( ",
+     " ) /)",
+     ")\\())",
+     "((_)\\"],
+    ["   ( ",
+     " (\\ (",
+     ")()\\)",
+     "/(_))"],
+    ["  (  ",
+     " )\\ )",
+     ")(//))",
+     "/(_))"]
+];
 
 function setBottom(){
+    bottomState ++;
+    if(bottomState >= bottomFires.length)bottomState = 0;
     var nFires = Math.round(bottom.offsetWidth / 32);
-    var lines = [
-        "   ) ",
-        " ( /(",
-        ")\\())",
-        "((_)\\"
-    ];
     var array = ["",""," ",""];
     for(var i = 0; i < nFires; i ++){
-        array[0] += lines[0];
-        array[1] += lines[1];
-        array[2] += lines[2];
-        array[3] += lines[3];
+        array[0] += bottomFires[bottomState][0];
+        array[1] += bottomFires[bottomState][1];
+        array[2] += bottomFires[bottomState][2];
+        array[3] += bottomFires[bottomState][3];
     }
     bottom.innerText = fireString(array);
 }
